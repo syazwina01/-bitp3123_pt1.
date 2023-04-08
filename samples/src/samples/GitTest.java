21649382206140b5c537ef87ce19ec10a3cb88f8
Syazwina <syazwinaahamed@gmail.com>
2023-04-08 12:24:11 +0800

    Initial Commit

--- samples/src/samples/GitTest.java
+++ samples/src/samples/GitTest.java
@@ -1,7 +1,6 @@
-package samples;
-
-public class GitTest{
-	public static void main(String[] args) {
-        System.out.println("Git Is Fun");
-}
-}
+/*
+ * package samples;
+ * 
+ * public class GitTest{ public static void main(String[] args) {
+ * System.out.println("Git Is Fun"); System.out.println("Git is cool"); } }
+ */
