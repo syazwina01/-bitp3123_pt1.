21577e051beb076af297d60a9cf9aaf688e38026
Syazwina <syazwinaahamed@gmail.com>
2023-04-08 12:32:09 +0800

    Initial Commit

--- samples/src/samples/Location.java
+++ samples/src/samples/Location.java
@@ -1,6 +1,7 @@
 package samples;
 import java.util.Scanner;
 class Student {
+private String name;
 private int age;
 public int getAge() {
 return age;
@@ -8,7 +9,14 @@ return age;
 public void setAge(int age) {
 this.age = age;
 }
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
 public void display() {
+System.out.println("name: "+getName());
 System.out.println("age: "+getAge());
 }
 }
@@ -16,10 +24,13 @@ public class Location{
 public static void main(String args[]) {
 //Reading values from user
 Scanner sc = new Scanner(System.in);
+System.out.println("Enter the name of the student: ");
+String name = sc.nextLine();
 System.out.println("Enter the age of the student: ");
 int age = sc.nextInt();
 //Calling the setter and getter methods
 Student obj = new Student();
+obj.setName(name);
 obj.setAge(age);
 obj.display();
 }
